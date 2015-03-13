aaec21e4fa8d85d156cf5d23b478a5849e6cbf78
Laura Cordoba <castelinolaura@gmail.com>
2015-03-13 14:01:20 -0300

    Clean code and Creation of seed elements

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+tate = Store.create(name: 'Tate', address: 'Rivadavia 322')
+rossetti = Store.create(name: 'Rosetti', address: 'Velez Sarsfield 600')
+
+Article.create(name: 'Hush Puppies', description: 'Zapato', price: 500, total_in_shelf: 20, total_in_vault: 100, store_id: tate.id)
+Article.create(name: 'Nike', description: 'Zapatillas', price: 800, total_in_shelf: 30, total_in_vault: 200, store_id: tate.id)
+Article.create(name: 'Adidas', description: 'Zapatillas', price: 700, total_in_shelf: 200.50, total_in_vault: 100.50, store_id: tate.id)
+
+Article.create(name: 'Puma', description: 'Zapatillas', price: 900, total_in_shelf: 25, total_in_vault: 100, store_id: rossetti.id)
+Article.create(name: 'Nike', description: 'Zapatillas', price: 800, total_in_shelf: 30, total_in_vault: 200, store_id: rossetti.id)
