7faefc25a7e2af5d2c34d6cd8ccceca7dc530b91
Laura Cordoba <castelinolaura@gmail.com>
2015-03-14 00:11:18 -0300

    wip

--- spec/api/shoes_api/stores_spec.rb
+++ spec/api/shoes_api/stores_spec.rb
@@ -9,7 +9,6 @@ describe ShoesApi::Stores do
 
       expect(response.status).to eq 200
       expect(JSON.parse(response.body)["total_elements"]).to eq 0
-      # expect(response.body).to eq "{\"stores\":[],\"success\":\"true\",\"total_elements\":0}"
     end
   end
 
@@ -22,6 +21,18 @@ describe ShoesApi::Stores do
 
       expect(response.status).to eq 200
       expect(JSON.parse(response.body)["total_elements"]).to eq 1
+    end
+  end
+
+  describe "GET /api/v1/services/stores" do
+    it "fails when user and password are wrong" do
+      store = FactoryGirl.create(:store)
+
+      get '/api/v1/services/stores', nil,
+          'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials('wrong_user', 'wrong_password')
+
+      expect(response.status).to eq 401
+      expect(JSON.parse(response.body)["error_msg"]).to eq 'Not Authorized'
     end
   end
 
